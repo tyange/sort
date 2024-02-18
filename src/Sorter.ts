e3ea1f96dd1f09674010b5bcb364a518ec43ebce
tyange <usun16@gmail.com>
2024-02-18 23:12:12 +0900

    define Sortable interface

--- src/Sorter.ts
+++ src/Sorter.ts
@@ -1,7 +1,13 @@
 import { NumbersCollections } from "./NumbersCollection";
 
+interface Sortable {
+  length: number;
+  compare(leftIndex: number, rightIndex: number): boolean;
+  swap(leftIndex: number, rightIndex: number): void;
+}
+
 export class Sorter {
-  constructor(public collection: NumbersCollections) {}
+  constructor(public collection: Sortable) {}
 
   sort(): void {
     const { length } = this.collection;
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollections } from "./NumbersCollection";
 
-const numbersCollection = new NumbersCollections([100000, 3, -5, 0]);
+const numbersCollection = new NumbersCollections([15, 3, -5, 0]);
 const sorter = new Sorter(numbersCollection);
 sorter.sort();
 console.log(numbersCollection.data);
