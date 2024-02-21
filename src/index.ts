cab4cdc67ad5151097edae46058f3937a2ebb121
tyange <usun16@gmail.com>
2024-02-22 08:02:36 +0900

    sorting characters

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollections } from "./NumbersCollection";
+import { CharactersCollection } from "./CharactersCollection";
 
-const numbersCollection = new NumbersCollections([15, 3, -5, 0]);
-const sorter = new Sorter(numbersCollection);
+// const numbersCollection = new NumbersCollections([15, 3, -5, 0]);
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
+// console.log(numbersCollection.data);
+
+const charactersCollection = new CharactersCollection("Xaayb");
+const sorter = new Sorter(charactersCollection);
 sorter.sort();
-console.log(numbersCollection.data);
+console.log(charactersCollection.data);
